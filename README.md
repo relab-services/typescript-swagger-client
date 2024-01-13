3bc588fe49c1aef96d3c744ae5441177c023a9c8
Sergey Zwezdin <sergey@zwezdin.com>
2024-01-13 10:12:23 +0300

    feat: Initial implementation

---
+++ README.md
@@ -0,0 +1,26 @@
+# @sergeyzwezdin/typescript-swagger-client
+
+Utility package to generate typescript client by Swagger/OpenAPI definition.
+
+## Usage
+
+1. `npm install --save-dev @sergeyzwezdin/typescript-swagger-client`
+2. Add command to `scripts` section of `package.json` file
+```json
+{
+  "name": "my-app",
+  "scripts": {
+    "swagger-client": "typescript-swagger-client --input @api/api.generated.json --output @api/generated"
+  }
+}
+3. `npm run swagger-client`
+
+Parameters of `typescript-swagger-client`:
+- `--input` - JSON-file path in your project that contains OpenAPI definition
+- `--output` - Folder for generated code
+
+```
+
+## License
+
+Released under [MIT](/LICENSE) by [Sergey Zwezdin](https://github.com/sergeyzwezdin).
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
+import copy from 'rollup-plugin-copy'
 import del from 'rollup-plugin-delete'
 import { dts } from 'rollup-plugin-dts'
 import external from 'rollup-plugin-peer-deps-external'
@@ -12,12 +13,22 @@ export default [
         output: [
             {
                 dir: 'lib',
-                format: 'cjs',
+                format: 'es',
                 name: 'lib',
                 preserveModules: true,
             },
         ],
-        plugins: [del({ targets: 'lib/*' }), external(), resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), terser()],
+        plugins: [
+            del({ targets: 'lib/*' }),
+            commonjs(),
+            resolve(),
+            external(),
+            typescript({ tsconfig: './tsconfig.json' }),
+            terser(),
+            copy({
+                targets: [{ src: 'src/templates', dest: 'lib' }],
+            }),
+        ],
     },
     {
         input: 'lib/types/index.d.ts',
